255c450285a57feda41dfe2b854894b304a877db
Claudio Antonius <antonius.claudio@gmail.com>
2020-04-22 01:13:20 +0700

    update endpoint - add cart - update cart - add authorization

---
+++ controllers/controllerCart.js
@@ -0,0 +1,101 @@
+const { Cart, Product } = require('../models');
+const { Op } = require('../models').Sequelize;
+class controllerCart {
+    static getCart (req, res, next) {
+        Cart.findAll({
+            where: {
+                CustomerId: req.CustomerId
+            }
+        })
+            .then((result) => {
+                res.status(200).json(result);
+            })
+            .catch((err) => {
+                next(err);
+            });
+    }
+
+    static addCart (req, res, next) {
+        let form = {
+            CustomerId: req.CustomerId,
+            ProductId: req.body.ProductId,
+            quantity: Number(req.body.quantity)
+        }
+        let idFindOne = null;
+        Product.findByPk(form.ProductId)
+            .then((result) => {
+                if (!result) {
+                    throw { msg:'Product not found!', status:404 }
+                } else {
+                    Cart.findOne({
+                        where: {
+                            [Op.and] : [
+                                {CustomerId: form.CustomerId},
+                                {ProductId: form.ProductId}
+                            ]
+                        }
+                    })
+                        .then((result) => {
+                            if (result) {
+                                idFindOne = result.id;
+                                Cart.update({quantity: (result.quantity + form.quantity)}, {
+                                    where: {
+                                        id: idFindOne
+                                    }
+                                })
+                                    .then((result) => {
+                                        Cart.findByPk(idFindOne)
+                                            .then((result) => {
+                                                res.status(200).json(result);
+                                            })
+                                            .catch((err) => {
+                                                next(err);
+                                            });
+                                    })
+                                    .catch((err) => {
+                                        next(err);
+                                    });
+                            } else {
+                                Cart.create(form)
+                                    .then((result) => {
+                                        res.status(201).json(result);
+                                    })
+                                    .catch((err) => {
+                                        next(err);
+                                    })
+                            }
+                        })
+                        .catch((err) => {
+                            next(err);
+                        });
+                }
+            })
+            .catch((err) => {
+                next(err);
+            });
+    }
+
+    static updateCart (req, res, next) {
+        let form = {
+            CustomerId: req.CustomerId,
+            id: req.body.id,
+            quantity: Number(req.body.quantity)
+        }
+        return Cart.update({quantity: form.quantity}, {
+            where: {
+                id: form.id
+            }
+        })
+            .then((result) => {
+                return Cart.findByPk(form.id)
+            })
+            .then((result) => {
+                res.status(200).json(result);
+            })
+            .catch((err) => {
+                next(err);
+            });
+    }
+}
+
+module.exports = controllerCart;
--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,15 @@ const router = require('express').Router();
 const controllerCustomer = require('../controllers/controllerCustomer');
 const errorHandler = require('../middlewares/errorHandler');
 const routerProduct = require('./routerProduct');
-
+const routerCart = require('./routerCart');
 
 router.post('/login', controllerCustomer.login);
 
 router.post('/register', controllerCustomer.register);
 
 router.use('/products', routerProduct);
+
+router.use('/carts', routerCart);
 
 router.use(errorHandler);
 
