145de634547a934e2da3c453a3e9337a5ec05b43
Claudio Antonius <antonius.claudio@gmail.com>
2020-04-23 13:31:29 +0700

    rapi-rapi

--- client/src/components/ListCheckoutProduct.vue
+++ client/src/components/ListCheckoutProduct.vue
@@ -1,21 +1,26 @@
 <template>
     <div class="columns is-multiline">
         <div class="column is-3">
-            {{checkoutProduct}}
+            {{checkoutProduct.name}}
         </div>
         <div class="column is-3">
+            {{checkoutProduct.price}}
         </div>
         <div class="column is-3">
+            {{checkoutProduct.quantity}}
         </div>
         <div class="column is-3">
+            {{total}}
         </div>
     </div>
 </template>
 <script>
 export default {
     props: ['checkoutProduct'],
-    methods: {
-        
+    computed: {
+        total: function() {
+            return (this.checkoutProduct.price * this.checkoutProduct.quantity);
+        }
     },
 }
 </script>
